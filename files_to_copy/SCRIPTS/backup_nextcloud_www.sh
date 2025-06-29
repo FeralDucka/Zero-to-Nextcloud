80f668f9924a4df20ce703df00c8242c4b772be4
FeralDucka <andreabrollo047@gmail.com>
2025-06-29 17:54:26 +0200

    Added verbose to backup_nextcloud_www.sh tar command

--- files_to_copy/SCRIPTS/backup_nextcloud_www.sh
+++ files_to_copy/SCRIPTS/backup_nextcloud_www.sh
@@ -5,6 +5,6 @@ DATE=$(date +"%Y-%m-%d__%H-%M-%S")
 FILE_NAME="nextcloud__www__${DATE}"
 
 # Make an archive of the web folder of Nextcloud
-tar -czf "/mnt/NEXTCLOUD_BACKUP/$FILE_NAME.tar.gz" "/var/www/nextcloud"
+tar -czvf "/mnt/NEXTCLOUD_BACKUP/$FILE_NAME.tar.gz" "/var/www/nextcloud"
 chmod 400 "/mnt/NEXTCLOUD_BACKUP/$FILE_NAME.tar.gz"
 chattr +i "/mnt/NEXTCLOUD_BACKUP/$FILE_NAME.tar.gz"
