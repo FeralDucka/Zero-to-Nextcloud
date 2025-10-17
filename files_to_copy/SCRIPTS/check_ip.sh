40a2d2a6cfbf57ea56f05718405a7979fd1b2ef3
FeralDucka <andreabrollo047@gmail.com>
2025-10-17 17:16:29 +0200

    Update check_ip.sh

--- files_to_copy/SCRIPTS/check_ip.sh
+++ files_to_copy/SCRIPTS/check_ip.sh
@@ -6,12 +6,16 @@ last_ip_file="/usr/local/sbin/last_ip.txt"
 if [[ ! -f "$last_ip_file" ]]; then
     touch $last_ip_file
 fi
-last_ip=$(cat "$last_ip_file")
+last_registered_ip=$(cat "$last_ip_file")
+current_duckdns_ip=$(curl -s "https://dns.google/resolve?name=$duckdns_domain.duckdns.org&type=A" | sed -n 's/.*"data":"\([0-9.]*\)".*/\1/p')
 current_ip=$(curl -s https://api.ipify.org)
-if [[ "$last_ip" == "$current_ip"  ]]; then
+if [[ "$last_registered_ip" == "$current_ip" ]] && [[ "$current_duckdns_ip" == "$current_ip" ]]; then
     echo "IP not changed"
+elif [[ "$current_duckdns_ip" != "$current_ip" ]]; then
+    echo "Wrong DuckDNS IP"
+    curl "$duckdns_url$current_ip"
 else
     echo "IP changed"
-    echo "$current_ip" > "$last_ip_file"
+    echo "$current_ip" > "$last_registered_ip"
     curl "$duckdns_url$current_ip"
 fi
